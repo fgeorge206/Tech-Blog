702660acebfe511982523b77f840f68bf376e466
Febin George <you@example.com>
2023-03-13 20:12:24 -0700

    updated

--- public/js/profile.js
+++ public/js/profile.js
@@ -1,6 +1,7 @@
 document.querySelector("#new-post-form").addEventListener("submit",e=>{
     e.preventDefault();
     const postObj = {
+        title:document.querySelector("#title-input").value,
         post:document.querySelector("#post-input").value
     }
     console.log(postObj)
